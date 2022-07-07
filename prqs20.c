fe51c30157ec20db3de7deb8b8562f2a3863762b
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-07-07 20:51:42 +0600

    odd number 5 to 50

---
+++ prqs20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    for (int i=5; i<=50;  i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d\n" , i);
+        }
+        
+    }
+    
+    return 0;
+}
